237580c82a51b6e8193c8f4f2f81c2960007350d
Haunter-123 <jhanishitback@gmail.com>
2021-09-30 20:53:34 +0530

    design

--- src/course.js
+++ src/course.js
@@ -4,27 +4,23 @@ import {useHistory} from "react-router-dom";
 
 
 function Course(props){
-    let History = useHistory();
 
-    const [img,setImg] =useState(props.img)
-    const [name,setName] =useState(props.name)
-    const [desc,setDesc] =useState(props.desc)
-    const [tags,setTags] = useState(props.tags)
+
 
 
 
     return(
         <div className='mainCourse'>
-            <img src={img} className="courseImg" />
-            <h2>{name}</h2>
-            <h4>{desc}</h4>
+            <img src={props.img} className="courseImg" />
+            <h2>{props.name}</h2>
+            <h4>{props.desc}</h4>
 
-            <p><i className='fa fa-tag'/>&nbsp;{tags}</p>
-            <button className="joinCourseBtn" onClick={(e)=>{window.location = `course?c=${name}`}}>Join</button>
+            <p><i className='fa fa-tag'/>&nbsp;{props.tags}</p>
+            <button className="joinCourseBtn" onClick={(e)=>{window.location = `course?c=${props.name}`}}>Join</button>
         </div>
     )
 
 
 }
-
+//do stuff
 export default Course;
--- src/courses.js
+++ src/courses.js
@@ -1,5 +1,7 @@
-import React from "react";
-import {useLocation} from "react-router-dom";
+import React, {useState} from "react";
+import {Link, useLocation} from "react-router-dom";
+import codeBg from "./img/code.jpg"
+import "./course.css"
 
 const Courses = require("./courses.json")
 function CourseTopDiv(){
@@ -7,10 +9,41 @@ function CourseTopDiv(){
     var cName = params.get("c")
 
     return(
-        <div>
+        <div id='main'>
 
-            <h2>{cName}</h2>
-            <p>{Courses[cName]["desc"]}</p>
+        <div id="topInfo">
+
+
+            <div id='imgDiv'>
+                <img src={codeBg} className="codeBg"/>
+            </div>
+
+            <div id='infoDiv'>
+                <h2>{cName}</h2>
+                <p>{Courses[cName]["desc"]}</p>
+            </div>
+
+
+        </div>
+
+        <div id="nav">
+            <button className="links active">
+                <Link to={useLocation()} style={{ textDecoration:'none',color:"white"}}>
+                    Who can join
+                </Link>
+            </button>
+
+            <button className="links">
+            <Link to={`dates/${cName}`} style={{ textDecoration: 'none',color:"black"}}>
+                Dates
+            </Link>
+            </button>
+        </div>
+
+        <div id='moreInfo'>
+
+        </div>
+
         </div>
     )
 }
