b4b4cc6c11512566481ce6293456c766d64b8ab5
smeh09 <shauryameh09@gmail.com>
2021-09-30 14:00:21 +0530

    added home app hero main in landing page of the coding bootcamp

--- src/homeAppHero.js
+++ src/homeAppHero.js
@@ -1,11 +1,21 @@
+import { Link } from 'react-router-dom'
 import './homeAppHeroMain.css'
 import React from 'react'
 
 export const HomeAppHero = () => {
   return (
     <div className='homeAppHeroMain'>
-      <div className='homeAppHero'>
-        re
+      <div className='homeAppHeroLeftArea'>
+        <h1 className='homeAppHeroHeading'>
+          A WEB DEV CODING BOOTCAMP FOR YOU
+        </h1>
+        <p className='homeAppHeroDesc'>
+          A coding bootcamp for beginners planning to learn web development. You will learn some technologies that will help you make websites, there will be projects too! So what are you waiting for let's get started! 
+        </p>
+        <Link className='homeAppHeroGetStarted' to='/courses'>Get started</Link>
+      </div>
+      <div className='homeAppHeroRightArea'>
+        <img className='homeAppHeroImage' src='https://img.freepik.com/free-vector/web-developers-courses-computer-programming-web-design-script-coding-study-computer-science-student-learning-interface-structure-components_335657-2542.jpg?size=338&ext=jpg' />
       </div>
     </div>
   )
